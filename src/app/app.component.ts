efcc7a71b3e0ff9a205aba191d39eddd59b71290
Michelle <michelle.lefebvre@valtech.com>
2020-12-27 12:15:58 -0500

    added todo items locally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
-  name:string = 'Michelle';
 
-  constructor() {
-    this.changeName('Marie');
-    console.log(123);
-  }
-  changeName(name:string):void {
-    this.name = name;
-  }
 }
---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // adding a list of todos
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: true
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false
+      }
+    ]
+  }
+
+}
